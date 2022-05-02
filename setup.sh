111af4ef2c1e39c468bfdd53c30300b64f93ff42
Jarosław Bąk <13832590+bonsoirq@users.noreply.github.com>
2022-05-02 19:50:56 +0200

    add rofi config

--- setup.sh
+++ setup.sh
@@ -15,15 +15,16 @@ ln -s .dotfiles/.nvmrc .nvmrc
 ln -s .dotfiles/.vimrc .vimrc
 
 if [ $(uname) = 'Linux' ]; then
-  rm .xinitrc .zprofile .Xresources \
+  rm .xinitrc .Xresources .zprofile \
     .config/alacritty/alacritty.yml .config/polybar/config.ini .config/polybar/launch.sh  \
-    .config/i3/config .config/cava/config &&
+    .config/i3/config .config/cava/config .config/rofi/config.rasi &&
   ln -s .dotfiles/.xinitrc .xinitrc
-  ln -s .dotfiles/.zprofile .zprofile
   ln -s .dotfiles/.Xresources .Xresources
+  ln -s .dotfiles/.zprofile .zprofile
   ln .dotfiles/.config/alacritty/alacritty.yml .config/alacritty/alacritty.yml
+  ln .dotfiles/.config/cava/config .config/cava/config
+  ln .dotfiles/.config/i3/config .config/i3/config
   ln .dotfiles/.config/polybar/config.ini .config/polybar/config.ini
   ln .dotfiles/.config/polybar/launch.sh .config/polybar/launch.sh 
-  ln .dotfiles/.config/i3/config .config/i3/config
-  ln .dotfiles/.config/cava/config .config/cava/config
+  ln .dotfiles/.config/rofi/config.rasi .config/rofi/config.rasi
 fi
