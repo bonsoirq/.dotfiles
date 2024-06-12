f887fcd2acda3355400c6bb83cf21ce4b954a263
Jarosław Bąk <13832590+bonsoirq@users.noreply.github.com>
2024-06-12 22:23:00 +0200

    nvim: init

--- setup.sh
+++ setup.sh
@@ -10,15 +10,17 @@ vim +PlugInstall +qall
 
 # Download zsh-vi-mode if not downloaded already
 [ ! -d "$HOME/.zsh-vi-mode" ] && git clone https://github.com/jeffreytse/zsh-vi-mode.git $HOME/.zsh-vi-mode
+# Download lazyvim if not downloaded already
+[ ! -d "$HOME/.config/nvim" ] && git clone https://github.com/LazyVim/starter $HOME/.dotfiles/.config/nvim
 
 # Link rc files
-rm -rf ~/{.gitconfig,.zshrc,.nvmrc,.vimrc} ~/.config/{alacritty,vifm} \
+rm -rf ~/{.gitconfig,.zshrc,.nvmrc,.vimrc} ~/.config/{alacritty,vifm,nvim} \
   && ln -s ~/.dotfiles/{.gitconfig,.zshrc,.nvmrc,.vimrc} ~/
-ln -s ~/.dotfiles/.config/{alacritty,vifm} ~/.config
+ln -s ~/.dotfiles/.config/{alacritty,vifm,nvim} ~/.config
 
 if [ $(uname) = 'Linux' ]; then
   rm -rf ~/{.xinitrc,.Xresources,.zprofile} \
-    .config/{awesome,cava,i3,picom,polybar,rofi} \
+    ~/.config/{awesome,cava,i3,picom,polybar,rofi} \
     && ln -s ~/.dotfiles/{.xinitrc,.Xresources,.zprofile} ~/
   ln -s ~/.dotfiles/.config/{awesome,cava,i3,picom,polybar,rofi} ~/.config
 fi
