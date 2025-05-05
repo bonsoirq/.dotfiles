242613dacb63d3f9b43a6b5190e11122e7ddf4fa
Jarosław Bąk <13832590+bonsoirq@users.noreply.github.com>
2025-05-05 16:17:13 +0200

    restore vanilla awesome config

--- shell/exports-Linux.bash
+++ shell/exports-Linux.bash
@@ -17,5 +17,3 @@ append_path "$HOME/.dotnet/tools"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-
-export XDG_CONFIG_HOME=~/.config 
